93dc4c13972c00a1a743578916cde5ec37742b41
Snova1054 <istar.dark@gmail.com>
2021-12-26 12:16:30 -0500

    changed tests, now working

--- contract/migrations/2_deploy_contracts.js
+++ contract/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var RocketElevators = artifacts.require("RocketElevators");
 
 module.exports = function(deployer) {
-  deployer.deploy(RocketElevators, "RocketElevatorsMundo", "REM", "ipfs://QmPK5DrogEbRs5vuQifj1UYY44CZZ3Lw6KZRQ7ufWt4f94/", "ipfs://QmVdweafQjvFWBfYMkubpuWYMiyQ1rME8WWv2j2LLLXsvz/hidden.json");
+  deployer.deploy(RocketElevators, "RocketElevatorsPepe", "REP", "ipfs://QmPK5DrogEbRs5vuQifj1UYY44CZZ3Lw6KZRQ7ufWt4f94/", "ipfs://QmVdweafQjvFWBfYMkubpuWYMiyQ1rME8WWv2j2LLLXsvz/hidden.json");
 };
--- contract/test/RocketTest.test.js
+++ contract/test/RocketTest.test.js
@@ -7,11 +7,11 @@ contract("RocketElevators", (accounts) => {
     });
     it("has a name", async () => {
       const name = await deployedContract.name();
-      assert.equal(name, "RocketElevatorsMundo")
+      assert.equal(name, "RocketElevatorsPepe")
     })
     it("has a symbol", async () => {
       const symbol = await deployedContract.symbol();
-      assert.equal(symbol, "REM")
+      assert.equal(symbol, "REP")
     })
     it("has correct max supply", async () => {
       const maxSupplyIs = await deployedContract.maxSupply();
@@ -24,8 +24,8 @@ contract("RocketElevators", (accounts) => {
     it("has a correct white list", async () => {
       const whitelistmember1 = await deployedContract.whiteListedAddresses(1);
       const whitelistmember2 = await deployedContract.whiteListedAddresses(2);
-      assert.equal(whitelistmember1, "0x49C99dB83eA1cDa354b718A4Be90f4B1C3Dc94A4")
-      assert.equal(whitelistmember2, "0xd1679bB3543e8aD195FF9f3Ac3436039bA389237")
+      assert.equal(whitelistmember1, "0xd1679bB3543e8aD195FF9f3Ac3436039bA389237")
+      assert.equal(whitelistmember2, "0x49C99dB83eA1cDa354b718A4Be90f4B1C3Dc94A4")
     })
   });
 });
